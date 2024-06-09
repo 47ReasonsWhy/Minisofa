fc4f8c319f6a58886cf10bbc143c42c0dec1554c
Marko Šelendić <118922023+47ReasonsWhy@users.noreply.github.com>
2024-06-09 22:10:34 +0200

    Everything else to support added leagues page and tournament details page (api, repo, viewmodel, string resources, navigation, etc.)

--- app/src/main/java/com/sofascoreacademy/minisofa/MainActivity.kt
+++ app/src/main/java/com/sofascoreacademy/minisofa/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.sofascoreacademy.minisofa
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import com.sofascoreacademy.minisofa.data.local.datastore.setThemeModeBasedOnPreference
+import com.sofascoreacademy.minisofa.data.local.datastore.themeModeFlow
 import com.sofascoreacademy.minisofa.data.model.Event
+import com.sofascoreacademy.minisofa.data.model.Tournament
 import com.sofascoreacademy.minisofa.databinding.ActivityMainBinding
+import com.sofascoreacademy.minisofa.ui.event_details_page.fragment.EventDetailsFragmentDirections
 import com.sofascoreacademy.minisofa.ui.home.HomeFragmentDirections
+import com.sofascoreacademy.minisofa.ui.home.HomeViewModel
+import com.sofascoreacademy.minisofa.ui.tournament_details_page.fragment.TournamentDetailsFragmentDirections
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     private lateinit var navController: NavController
+
+    private val homeViewModel: HomeViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,10 +32,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        lifecycleScope.launch {
+            themeModeFlow.collect {
+                setThemeModeBasedOnPreference()
+            }
+        }
+
+        homeViewModel.observeDateFormatPreference(lifecycleScope)
     }
 
-    fun navigateToEventDetails(event: Event) {
-        val action = HomeFragmentDirections.navigateToEventDetails(event)
+    fun navigateToEventDetailsFromHome(event: Event) {
+        val action = HomeFragmentDirections.navigateToEventDetailsFromHome(event)
+        navController.navigate(action)
+    }
+
+    fun navigateToTournamentDetailsFromHome(tournament: Tournament) {
+        val action = HomeFragmentDirections.navigateToTournamentDetailsFromHome(tournament)
+        navController.navigate(action)
+    }
+
+    fun navigateToEventDetailsFromTournamentDetails(event: Event) {
+        val action = TournamentDetailsFragmentDirections.navigateToEventDetailsFromTournamentDetails(event)
+        navController.navigate(action)
+    }
+
+    fun navigateToTournamentDetailsFromEventDetails(tournament: Tournament) {
+        val action = EventDetailsFragmentDirections.navigateToTournamentDetailsFromEventDetails(tournament)
         navController.navigate(action)
     }
 
@@ -36,4 +70,5 @@ class MainActivity : AppCompatActivity() {
     fun navigateBack() {
         navController.popBackStack()
     }
+
 }
--- app/src/main/java/com/sofascoreacademy/minisofa/data/remote/ApiService.kt
+++ app/src/main/java/com/sofascoreacademy/minisofa/data/remote/ApiService.kt
@@ -3,21 +3,56 @@ package com.sofascoreacademy.minisofa.data.remote
 import com.sofascoreacademy.minisofa.data.remote.response.EventResponse
 import com.sofascoreacademy.minisofa.data.remote.response.IncidentResponse
 import com.sofascoreacademy.minisofa.data.remote.response.SportResponse
+import com.sofascoreacademy.minisofa.data.remote.response.TournamentResponse
+import com.sofascoreacademy.minisofa.data.remote.response.TournamentStandingsResponse
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
+
+    // https://academy-backend.sofascore.dev/api/docs#tag/Sport/operation/get_app_sports
     @GET("sports")
     suspend fun getSports(): List<SportResponse>
 
+
+    // https://academy-backend.sofascore.dev/api/docs#tag/Sport/operation/get_app_sport_events
     @GET("sport/{slug}/events/{date}")
-    suspend fun getEvents(
+    suspend fun getEventsForSportAndDate(
         @Path("slug") slug: String,
         @Path("date") date: String
     ): List<EventResponse>
 
+    // https://academy-backend.sofascore.dev/api/docs#tag/Sport/operation/get_app_sport_tournaments
+    @GET("/sport/{slug}/tournaments")
+    suspend fun getTournamentsForSport(
+        @Path("slug") slug: String
+    ): List<TournamentResponse>
+
+
+    // https://academy-backend.sofascore.dev/api/docs#tag/Event/operation/get_app_event_incidents
     @GET("event/{id}/incidents")
     suspend fun getIncidents(
         @Path("id") id: Int
     ): List<IncidentResponse>
+
+
+    // https://academy-backend.sofascore.dev/api/docs#tag/Tournament/operation/get_app_tournaments_events
+    @GET("tournament/{id}/events/next/{page}")
+    suspend fun getFutureEventsForTournament(
+        @Path("id") id: Int,
+        @Path("page") page: Int
+    ): List<EventResponse>
+    @GET("tournament/{id}/events/last/{page}")
+    suspend fun getPastEventsForTournament(
+        @Path("id") id: Int,
+        @Path("page") page: Int
+    ): List<EventResponse>
+
+
+    // https://academy-backend.sofascore.dev/api/docs#tag/Tournament/operation/get_app_tournaments_standings
+    @GET("tournament/{id}/standings")
+    suspend fun getStandings(
+        @Path("id") id: Int
+    ): List<TournamentStandingsResponse>
+
 }
